e752a3ef156f4ca39bf83cea761e706d543e079a
Mehmet Ali Baykara <mehmetali.baykara@encoway.de>
2020-12-16 22:51:05 +0100

    Ngram

--- ngram.go
+++ ngram.go
@@ -5,40 +5,65 @@ import (
 	"io/ioutil"
 	"os"
 	"regexp"
+	"strings"
+	"unicode"
 
 	"github.com/bbalet/stopwords"
 )
 
 
 func main() {
-	if len(os.Args) < 2 {
+ 	if len(os.Args) < 2 {
 		fmt.Println("Provide a proper filename")
 		os.Exit(1)
 	}
+	 
 	d, err := ioutil.ReadFile(os.Args[1])
 	if err != nil {
-		fmt.Println("The file name is not available: ", os.Args[1])
+		fmt.Println("Given filename is not exist: ", os.Args[1])
 		panic(err)
 	}
 
-	data :=tokenize(string(d))
-	ngram(data,2)
-
+	fmt.Println(kgram(string(d),2))
 }
 
 func tokenize(data string) map[string]int{
-	//Data will be cleaned or not
-	cleanData := stopwords.CleanString(string(data),"de",true)
-	fmt.Println(cleanData)
+
 	a := regexp.MustCompile(` `)
 	result := a.Split(data,-1)
 	words := result
-
 	m := make(map[string]int)
 	for _, word := range words {
 		m[word]++
 	}
 	return m
-		//if _, ok := m[word];ok { m[word]++}
 }
-
+/*
+ following method is inspired by https://github.com/rafatbiin/gongram/blob/master/ngram.go
+*/
+func puncAndStopword(data string) string {
+	data = strings.TrimSpace(data)
+	data = stopwords.CleanString(data,"de",true)
+	builder := strings.Builder{}
+	builder.Grow(len(data))
+	for _, c := range data {
+		if !unicode.IsPunct(c) {
+			builder.WriteRune(c)
+		}
+	}
+	return builder.String()
+}
+ 
+func kgram(data string, k int) ([]string, error) {
+	if k > len(data) {
+		fmt.Println("The given kgram is bigger than corpus")
+	}
+	data = puncAndStopword(data)
+	s := strings.Split(data," ")
+	result := []string{}
+	for i := 0 ; i<(len(s)-k+1);i++ {
+		ngram := strings.Join(s[i:i+k], " ")
+		result = append(result, ngram)
+	}
+	return result, nil
+}
